3ebc936d09b42fcbc85647f40792bbf060951a0f
Bradley Allen <BradleyA@users.noreply.github.com>
2017-12-17 15:38:52 -0600

    Update cluster-shutdown.sh

--- cluster-shutdown/cluster-shutdown.sh
+++ cluster-shutdown/cluster-shutdown.sh
@@ -2,7 +2,8 @@
 #	%W% %G% %U%
 #	set -x
 #	set -v
+DOMAIN="cptx86.com"
 for NODE in one two three four five six ; do
-	echo rpi3b-$NODE;
-	ssh $USER@rpi3b-$NODE.cptx86.com 'sudo shutdown -f now';
+	echo rpi3b-$NODE.$DOMAIN;
+	ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
 done
