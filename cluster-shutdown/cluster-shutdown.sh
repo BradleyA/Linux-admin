c947716fb3f5f98f23c0750f3bf7279847ed1059
Bradley Allen <allen.bradley@ymail.com>
2017-12-17 17:19:29 -0600

    updated script to skip local host until after the loop is completed and correct test on the if statement

--- cluster-shutdown/cluster-shutdown.sh
+++ cluster-shutdown/cluster-shutdown.sh
@@ -2,8 +2,15 @@
 #	%W% %G% %U%
 #	set -x
 #	set -v
+#	ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
+REMOTECOMMAND="shutdown -f now"
 DOMAIN="cptx86.com"
+LOCALHOST=`hostname -f`
+#
 for NODE in one two three four five six ; do
 	echo rpi3b-$NODE.$DOMAIN;
-	ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
+	if [ "$LOCALHOST" != "rpi3b-$NODE.$DOMAIN" ]; then
+		ssh $USER@rpi3b-$NODE.$DOMAIN $REMOTECOMMAND;
+	fi
 done
+$REMOTECOMMAND
