b0af5aed80f2d7fdc1c35bc8ccad38460b23911a
Bradley Allen <allen.bradley@ymail.com>
2017-12-20 21:49:24 -0600

    add version to file from markit

--- cluster-shutdown/cluster-shutdown.sh
+++ cluster-shutdown/cluster-shutdown.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#	%W% %G% %U%
+#	cluster-shutdown.sh	1.0	2017-12-20_21:48:47_CST uadmin rpi3b-two.cptx86.com
+#	add version to file from markit
+#
 #	set -x
 #	set -v
 #	ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
+#
 REMOTECOMMAND="shutdown -f now"
 DOMAIN="cptx86.com"
 LOCALHOST=`hostname -f`
@@ -10,7 +13,7 @@ LOCALHOST=`hostname -f`
 for NODE in one two three four five six ; do
 	echo rpi3b-$NODE.$DOMAIN;
 	if [ "$LOCALHOST" != "rpi3b-$NODE.$DOMAIN" ]; then
-		ssh $USER@rpi3b-$NODE.$DOMAIN $REMOTECOMMAND;
+		ssh -tt $USER@rpi3b-$NODE.$DOMAIN 'stty raw -echo;sudo shutdown -f now';
 	fi
 done
-$REMOTECOMMAND
+sudo $REMOTECOMMAND
