5c6aa99d3fc7536c67deaef71f4c9d32f49e2701
BradleyA <allen.bradley@ymail.com>
2018-02-22 08:59:56 -0600

    cluster-command complete display-help closes #4

--- cluster-command/cluster-command.sh
+++ cluster-command/cluster-command.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# 	cluster-command.sh	1.6.36	2018-02-22_08:59:07_CST uadmin six-rpi3b.cptx86.com 1.5 
+# 	   cluster-command complete display-help closes #4 
 # 	cluster-command.sh	1.5.35	2018-02-22_08:16:57_CST uadmin six-rpi3b.cptx86.com 1.4 
 # 	   cluster-command.sh set default location for host file closes #3 
 # 	cluster-command.sh	1.3.33	2018-02-21_21:12:39_CST uadmin six-rpi3b.cptx86.com 1.2-2-gde89b7b 
 # 	   cluster-shutdown.sh use host list file closes #1 support several commands closes #2 
-# 	cluster-command.sh	1.2.30	2018-02-21_13:36:37_CST uadmin six-rpi3b.cptx86.com 1.1 
-# 	   comment out x86 hosts so I will not shutdown production cluster during testing; supports #1 
-# 	cluster-shutdown.sh	1.0.22	2018-02-21_12:28:36_CST uadmin six-rpi3b.cptx86.com v0.1-21-g8174b37 
-# 	   remove comment for enhancements and move to issues in github 
 #
 #	set -x
 #	set -v
@@ -15,23 +13,30 @@
 #	   ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
 ###		
 display_help() {
-echo -e "\n${0} - remote cluster system adminstration"
+echo -e "\n${0} - remote cluster system adminstration tool"
 echo -e "\nUSAGE\n   ${0} [<options>] [<path>/<hostfile.txt>]"
 echo    "   ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
-echo -e "\nDESCRIPTION\nRun one command from a set of commands on a group of systems found in cluster-command.txt file."
+echo -e "\nDESCRIPTION\nThis script runs a command from a set of predefined commands on hosts."
+echo    "The hosts are found in a file with one host per line.  Lines that begin a #"
+echo    "are comments.  The defualt location for cluster-command.txt file is"
+echo    "/usr/local/bin.  A different path and file can be entered on the command line"
+echo    "as the second argument."
 echo -e "\nOPTIONS "
-echo    "   docker-version - docker version | grep -m 1 'Version:'"
-echo    "   shutdown - sudo shutdown -f now"
-echo    "   reboot - sudo reboot"
-echo    "   update - sudo apt-get update"
-echo    "   upgrade - sudo apt-get upgrade --assume-yes"
-echo    "   dist-upgrade - sudo apt-get dist-upgrade --assume-yes"
-echo    "   showhold - apt-mark showhold"
-echo    "   OS - lsb_release -d"
-echo    "   cpu - lscpu"
-echo    "   require-reboot - if [ -f /var/run/reboot-required ]; then echo 'reboot required' ; else echo 'no reboot required' ; fi"
-echo    "   require-upgrade - /usr/lib/update-notifier/apt-check --human-readable"
-echo    "   upgrade-package - apt-get upgrade --simulate | grep -vE 'Conf|Inst'"
+echo    "   List of predefind commands:"
+echo    "      docker-version - docker version | grep -m 1 'Version:'"
+echo    "      shutdown       - sudo shutdown -f now"
+echo    "      reboot         - sudo reboot"
+echo    "      update         - sudo apt-get update"
+echo    "      upgrade        - sudo apt-get upgrade --assume-yes"
+echo    "      dist-upgrade   - sudo apt-get dist-upgrade --assume-yes"
+echo    "      showhold       - apt-mark showhold"
+echo    "      OS             - lsb_release -d"
+echo    "      cpu            - lscpu"
+echo    "      require-reboot - if [ -f /var/run/reboot-required ]; then echo 'reboot"
+echo    "                       required' ; else echo 'no reboot required' ; fi"
+echo    "      require-upgrade - /usr/lib/update-notifier/apt-check --human-readable"
+echo    "      upgrade-package - apt-get upgrade --simulate | grep -vE 'Conf|Inst'"
+echo    "   hostfile - default /usr/local/bin/cluster-command.txt"
 echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/pi-scripts/tree/master/cluster-command"
 echo -e "\nEXAMPLES\n   Shutdown raspberry pi clusters\n\t${0} shutdown\n"
 }
@@ -101,6 +106,7 @@ esac
 #
 for NODE in ${REMOTEHOST} ; do
 	echo -e "\n${0} ${LINENO} [INFO]:	${NODE}" ;
+#	Check if host is online ?
 	if [ "${LOCALHOST}" != " ${NODE}" ] ; then
 		ssh -tt ${USER}@${NODE} ${REMOTECOMMAND} ;
 	fi
