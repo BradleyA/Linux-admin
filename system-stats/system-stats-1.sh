961f6919545797459ea91d90e9343dc7d57a433f
BradleyA <allen.bradley@ymail.com>
2018-02-21 21:24:22 -0600

    ruff draft addition display_help cpu-temperature.sh & system-stats-1.sh

--- system-stats/system-stats-1.sh
+++ system-stats/system-stats-1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	../system-stats/system-stats-1.sh	1.4.34	2018-02-21_21:22:51_CST uadmin six-rpi3b.cptx86.com 1.3 
+# 	   ruff draft addition display_help cpu-temperature.sh & system-stats-1.sh 
 #	system-stats-1.sh	1.0	2017-12-20_22:12:37_CST uadmin rpi3b-two.cptx86.com
 #	initial version
 #
@@ -6,7 +8,25 @@
 #       set -v
 #
 #   	evaluate different states for raspberry pi
-#
+###             
+display_help() {
+echo -e "\n${0} - >>> NEED TO COMPLETE THIS SOON, ONCE I KNOW HOW IT IS GOING TO WORK :-) <<<"
+echo -e "\nUSAGE\n   ${0}"
+echo    "   ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
+echo -e "\nDESCRIPTION\nXXXXXX "
+echo -e "\nOPTIONS "
+echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/pi-scripts/tree/master/cpu-temperature"
+echo -e "\nEXAMPLES\n   XXXXXX \n\t${0} XXXXXX\n"
+}
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
+        display_help
+        exit 0
+fi
+if [ "$1" == "--version" ] || [ "$1" == "-v" ] ||  [ "$1" == "version" ]  ; then
+        head -2 ${0} | awk {'print$2"\t"$3'}
+        exit 0
+fi
+###
 echo "Hostname =	" `hostname`
 
 CPUTEMP=$(/usr/bin/vcgencmd measure_temp | \
