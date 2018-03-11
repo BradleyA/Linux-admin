0e7a9582efe137026770d269a5d9187a74ca30d4
BradleyA <allen.bradley@ymail.com>
2018-03-10 20:57:24 -0600

    cluster-command.txt mv to SYSTEMS file and change location to /usr/local/data/cluster-1 closes #9

--- cluster-command/cluster-command.sh
+++ cluster-command/cluster-command.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
+# 	cluster-command.sh  1.14.77  2018-03-10_20:54:45_CST  https://github.com/BradleyA/Linux-admin  uadmin  six-rpi3b.cptx86.com 1.13-16-gd9756b6  
+# 	   cluster-command.txt mv to SYSTEMS file and change location to /usr/local/data/cluster-1 closes #9 
 # 	cluster-command.sh	1.13.60	2018-02-23_16:24:00_CST uadmin one-rpi3b.cptx86.com 1.12-8-gdb44df7 
 # 	   debug new commands on two cluster 
-# 	cluster-command.sh	1.11.47	2018-02-22_16:43:22_CST uadmin six-rpi3b.cptx86.com 1.10 
-# 	   cluster-command determine if host is on-line close #6 
-# 	cluster-command.sh	1.10.46	2018-02-22_16:03:46_CST uadmin six-rpi3b.cptx86.com 1.9 
-# 	   added more commands / debugging close #5 cluster-command determine if host is on-line testing #6 
-# 	cluster-command.sh	1.9.45	2018-02-22_15:12:28_CST uadmin six-rpi3b.cptx86.com 1.8 
-# 	   updated README, completed testing, debugging, closes #6 
-# 	cluster-command.sh	1.8.44	2018-02-22_13:44:37_CST uadmin six-rpi3b.cptx86.com 1.7-3-g37f1656 
-# 	   add docker-release 
 #
 #	set -x
 #	set -v
@@ -18,14 +12,14 @@
 ###		
 display_help() {
 echo -e "\n${0} - remote cluster system adminstration tool"
-echo -e "\nUSAGE\n   ${0} [<options>] [<path>/<hostfile.txt>]"
+echo -e "\nUSAGE\n   ${0} [<predefind-command>] [<path>/SYSTEMS]"
 echo    "   ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
 echo -e "\nDESCRIPTION\nThis script runs a command from a set of predefined commands on hosts."
 echo    "The hostnames of the hosts are found in a file with one hostname per line."
 echo    "Lines in this file that begin with a # are comments.  The defualt"
-echo    "location for this cluster command host file, cluster-command.txt, is"
-echo    "/usr/local/bin.  A different path and cluster command host file can be"
-echo    "entered on the command line as the second argument."
+echo    "location for this cluster command host file, SYSTEMS, is"
+echo    "/usr/local/data/cluster-1/.  A different path and cluster command host file"
+echo    "can be entered on the command line as the second argument."
 echo -e "\nOPTIONS "
 echo    "   List of predefind commands:"
 echo    "      shutdown       - sudo shutdown -f now"
@@ -63,7 +57,7 @@ echo    "                       required' ; else echo 'no reboot required' ; fi"
 echo    "      require-upgrade - /usr/lib/update-notifier/apt-check --human-readable" # not sure this is the correct command becasue one-rpi3b stated no upgrade but then did eight upgrades
 echo    "      upgrade-package - apt-get upgrade --simulate | grep -vE 'Conf|Inst'"
 echo    "                        apt list --upgradeable -> does not work on Ubuntu 14.04"
-echo    "   HOSTFILE     Other file with hostnames, default /usr/local/bin/cluster-command.txt"
+echo    "   HOSTFILE     File with hostnames, default /usr/local/data/cluster-1/SYSTEMS"
 echo    "   SSHPORT      SSH server port, default is port 22"
 echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/pi-scripts/tree/master/cluster-command"
 echo -e "\nEXAMPLES\n   Shutdown raspberry pi clusters\n\t${0} shutdown\n"
@@ -79,8 +73,8 @@ fi
 ###
 #	execpt only commands in case statement
 REMOTECOMMAND=${1:-""}
-#	open issues :add argument or flag argument for a single host and not use cluster-command.txt file to execute a new command or a commad defined in this file
-HOSTFILE=${2:-"/usr/local/bin/cluster-command.txt"}
+#	open issues :add argument or flag argument for a single host and not use SYSTEMS file to execute a new command or a commad defined in this file
+HOSTFILE=${2:-"/usr/local/data/cluster-1/SYSTEMS"}
 SSHPORT=${3:-22}
 LOCALHOST=`hostname -f`
 BOLD=$(tput bold)
