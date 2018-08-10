f3055c5bdc8e188011a048fb6c2919c86c17614c
Bradley Allen <allen.bradley@ymail.com>
2018-08-10 14:55:56 -0500

    remote -p 22 because ssh port number is controlled by ~/.ssh/config file

--- cluster-command/cluster-command.sh
+++ cluster-command/cluster-command.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
+# 	cluster-command.sh  2.00.107  2018-08-10_14:54:54_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.21  
+# 	   remote -p 22 because ssh port number is controlled by ~/.ssh/config file 
 # 	cluster-command/cluster-command.sh  1.21.106  2018-07-30_19:07:28_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.20  
 # 	   add port option -p to ssh command 
-# 	cluster-command.sh  1.20.105  2018-07-30_18:22:11_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.19  
-# 	   poweroff did not turn off the power on the raspberry pi close #11 
-# 	cluster-command.sh  1.19.104  2018-07-30_17:28:12_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.18  
-# 	   test to see if these option will power off the Raspberry pi #11 
-# 	cluster-command.sh  1.18.103  2018-07-30_12:14:43_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.17-2-g679ac99  
-# 	   #11 changed order of options, updated help, added poweroff 
-# 	cluster-command.sh  1.17.100  2018-07-27_18:33:29_CDT  https://github.com/BradleyA/Linux-admin  uadmin  three-rpi3b.cptx86.com 1.16-8-g6229d6b  
-# 	   change default directory name to /usr/local/data/us-tx-cluster-1/SYSTEMS to support scaling this process 
-# 	cluster-command.sh  1.16.91  2018-03-18_13:17:40_CDT  https://github.com/BradleyA/Linux-admin  uadmin  six-rpi3b.cptx86.com 1.15  
-# 	   added require-update to require-upgrade 
-# 	cluster-command.sh  1.15.90  2018-03-14_21:05:05_CDT  https://github.com/BradleyA/Linux-admin  uadmin  six-rpi3b.cptx86.com 1.14-12-g2c16a3b  
-# 	   modify command download in README 
-# 	cluster-command.sh  1.14.77  2018-03-10_20:54:45_CST  https://github.com/BradleyA/Linux-admin  uadmin  six-rpi3b.cptx86.com 1.13-16-gd9756b6  
-# 	   cluster-command.txt mv to SYSTEMS file and change location to /usr/local/data/cluster-1 closes #9 
-#
+###
+#	administration cluster commands for Raspberry Pi and x86 clusters
+#	   ssh $USER@$NODE-rpi3b.$DOMAIN 'sudo shutdown -f now';
+###
+DEBUG=0                 # 0 = debug off, 1 = debug on
 #	set -x
 #	set -v
-#
-#	administration cluster commands for Raspberry Pi and x86 clusters
-#	   ssh $USER@rpi3b-$NODE.$DOMAIN 'sudo shutdown -f now';
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 ###		
 display_help() {
-echo -e "\n${0} - remote cluster system adminstration tool"
-echo -e "\nUSAGE\n   ${0} [<PREDEFINED-COMMAND>] [<SSHPORT>] [<path>/<HOSTFILE>]"
+echo -e "\n${NORMAL}${0} - remote cluster system adminstration tool"
+echo -e "\nUSAGE\n   ${0} [<PREDEFINED-COMMAND>] [<path>/<HOSTFILE>]"
 echo    "   ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
 echo -e "\nDESCRIPTION\nThis script runs a command from a set of predefined commands on hosts."
 echo    "The hostnames of the hosts are found in a file with one FQDN or IP address per"
@@ -68,10 +59,9 @@ echo    "      dist-upgrade   - sudo apt-get dist-upgrade --assume-yes"
 echo    "      autoremove     - sudo apt-get autoremove --assume-yes"
 echo    "      require-reboot - if [ -f /var/run/reboot-required ]; then echo 'reboot"
 echo    "                       required' ; else echo 'no reboot required' ; fi"
-echo    "      require-upgrade - /usr/lib/update-notifier/apt-check --human-readable" # not sure this is the correct command becasue one-rpi3b stated no upgrade but then did eight upgrades
+echo    "      require-upgrade - /usr/lib/update-notifier/apt-check --human-readable" # >>> not sure this is the correct command becasue one-rpi3b stated no upgrade but then did eight upgrades
 echo    "      upgrade-package - apt-get upgrade --simulate | grep -vE 'Conf|Inst'"
 echo    "                        apt list --upgradeable -> does not work on Ubuntu 14.04"
-echo    "   SSHPORT      SSH server port, default is port 22"
 echo    "   HOSTFILE     File with hostnames, default /usr/local/data/us-tx-cluster-1/SYSTEMS"
 echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/pi-scripts/tree/master/cluster-command"
 echo -e "\nEXAMPLES\n   Shutdown raspberry pi clusters\n\t${0} shutdown\n"
@@ -80,7 +70,7 @@ if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" =
         display_help
         exit 0
 fi
-if [ "$1" == "--version" ] || [ "$1" == "-v" ] ||  [ "$1" == "version" ]  ; then
+if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
         head -2 ${0} | awk {'print$2"\t"$3'}
         exit 0
 fi
@@ -88,11 +78,8 @@ fi
 #	execpt only commands in case statement
 REMOTECOMMAND=${1:-""}
 #	open issues :add argument or flag argument for a single host and not use SYSTEMS file to execute a new command or a commad defined in this file
-SSHPORT=${2:-22}
-HOSTFILE=${3:-"/usr/local/data/us-tx-cluster-1/SYSTEMS"}
+HOSTFILE=${2:-"/usr/local/data/us-tx-cluster-1/SYSTEMS"}
 LOCALHOST=`hostname -f`
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
 #       Check for ${HOSTFILE} file
 if [ ! -e ${HOSTFILE} ] ; then
         echo -e "${0} ${LINENO} [WARN]:        ${HOSTFILE} NOT found"   1>&2
@@ -209,12 +196,7 @@ esac
 for NODE in ${REMOTEHOST} ; do
 	echo -e "\n${BOLD}  -->  ${NODE}${NORMAL}	->${REMOTECOMMAND}<-" 
 	if [ "${LOCALHOST}" != "${NODE}" ] ; then
-#       Check if ${REMOTEHOST} is available on port ${SSHPORT}
-		if $(nc -z  ${NODE} ${SSHPORT} >/dev/null) ; then
-			ssh -t ${USER}@${NODE} -p ${SSHPORT} ${REMOTECOMMAND} 
-		else
-        		echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARN${NORMAL}]:	${NODE} not responding on port ${SSHPORT}.\n"	1>&2
-		fi
+		ssh -t ${USER}@${NODE} ${REMOTECOMMAND} 
 	else
 		eval ${REMOTECOMMAND}
 	fi
