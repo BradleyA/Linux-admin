a9f1bcb46f68ca7dca0cfe01adcc595f0674cc5f
Bradley Allen <allen.bradley@ymail.com>
2019-08-01 23:26:27 -0500

    github-repository/parse.repository.data.sh added design notes

--- github-repository/parse.repository.data.sh
+++ github-repository/parse.repository.data.sh
@@ -1,4 +1,14 @@
 
+# 	github-repository/parse.repository.data.sh  2.65.240  2019-08-01T23:26:27.358500-05:00 (CDT)  https://github.com/BradleyA/Linux-admin  uadmin  two-rpi3b.cptx86.com 2.64  
+# 	   github-repository/parse.repository.data.sh added design notes 
 # 	github-repository/parse.repository.data.sh  2.64.239  2019-08-01T23:19:27.795284-05:00 (CDT)  https://github.com/BradleyA/Linux-admin  uadmin  two-rpi3b.cptx86.com 2.63  
 # 	   github-repository/parse.repository.data.sh added file 
 ###
+#       
+#       need to write a parser
+#               to parse data/owner.repositry-date files into a comma separated values text files (.csv) in /usr/local/data/github/owner/repository/RAW/
+#               use .csv data or somthing to create an image or .png file
+#               push images to github owner/repository/images/(clones.png,views.png, popular.referrers.list, popular.paths.list)
+#               link images to github owner/repository/README.md page
+#        .... NOTE to self need data first and dont want to waste time creating test data
+
